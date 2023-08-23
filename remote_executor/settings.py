0a715a49d8ee8c3e7b40dc79d5f30da59e66bde3
Alexey Alexandrov <alexandrov-iu8@mail.ru>
2023-08-23 10:28:08 +0300

    Update settings.py

--- remote_executor/settings.py
+++ remote_executor/settings.py
@@ -3,38 +3,41 @@ from pathlib import Path
 
 import dotenv
 
+# Trying to get credentials from environment variables.
 dotenv.load_dotenv('.env')
+HOSTNAME = os.environ.get('HOSTNAME', default=None)
+USERNAME = os.environ.get('USERNAME', default=None)
+PASSWORD = os.environ.get('PASSWORD', default=None)
 
 # Base parameters.
 APPLICATION_NAME = 'RemotEXEcutor'
 BASE_DIR = Path(__file__).resolve().parent
 
+# Explicit pass the encoding type for I/O operations. 
+CONFIG_ENCODING = 'UTF-8'
+
 # Configure logo for CLI.
 LOGO_PATH = Path(BASE_DIR / 'logo.txt')
-APPLICATION_LOGO = LOGO_PATH.read_text('utf-8') if LOGO_PATH.exists() else APPLICATION_NAME
+APPLICATION_LOGO = LOGO_PATH.read_text(CONFIG_ENCODING) if LOGO_PATH.exists() else APPLICATION_NAME
 
-# Configure logging.
+# Logging.
 LOG_LEVEL = 'DEBUG'
 LOG_TO_FILES = True
 LOG_DIR = BASE_DIR / 'logs'
 
-HOSTNAME = os.environ.get('HOSTNAME', default=None)
-USERNAME = os.environ.get('USERNAME', default=None)
-PASSWORD = os.environ.get('PASSWORD', default=None)
-
+# Path to main config file with transport settings.
 MAIN_CONFIG_PATH = BASE_DIR.joinpath('config.ini')
-CONFIG_ENCODING = 'UTF-8'
 
-# Configure local configs.
+# Local configs' section names.
 LOCAL_CONFIG_NAME = 'remote_exec.yaml'
 PROGRAM_KEY = 'PROGRAM'
 DESCRIPTION_KEY = 'DESCRIPTION'
 PRE_EXEC_KEY = 'PRE_EXEC'
 BODY_KEY = 'BODY'
 POST_EXEC_KEY = 'POST_EXEC'
-
+# Paths to executables' directories.
 PROGRAMS_DIR = BASE_DIR.parent / 'programs'
 PROGRAMS_NIX_DIR = PROGRAMS_DIR / 'nix'
 PROGRAMS_WINDOWS_DIR = PROGRAMS_DIR / 'windows'
-
+# Path for uploading results of execution on remote host.
 RESULTS_DIR = BASE_DIR.parent / 'results'
